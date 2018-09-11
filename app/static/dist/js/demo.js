ccee520a72304adfbf29aa8ef5ada063f14bb4fc
Neiell Care L. Paradiang <koneb2013@gmail.com>
2018-09-12 01:16:46 +0800

    added filters on data tables

--- app/static/dist/js/demo.js
+++ app/static/dist/js/demo.js
@@ -10,11 +10,34 @@ $(function () {
     /**
      * Get access to plugins
      */
+    $('#dynamic-table thead tr').clone(true).appendTo( '#dynamic-table thead' );
 
     $('.selectClient').select2();
     $('.selectEmployee').select2();
     $('#dynamic-table').DataTable({
-        "ordering": false
+        ordering: false,
+        orderCellsTop: true,
+        fixedHeader: true,
+        initComplete: function () {
+            this.api().columns().every( function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.header()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+        }
     });
 
     $('[data-toggle="control-sidebar"]').controlSidebar();
